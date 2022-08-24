daa34f0c210128dd9f56043bfd2ea4b73ed94ec2
ukjin.jang <ujjang0605@gmail.com>
2022-08-24 10:17:25 +0900

    전략 패턴 (Strategy Pattern)

---
+++ src/test/java/hello/advanced/trace/strategy/ContextV1Test.java
@@ -0,0 +1,98 @@
+package hello.advanced.trace.strategy;
+
+import hello.advanced.trace.strategy.code.strategy.ContextV1;
+import hello.advanced.trace.strategy.code.strategy.Strategy;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic1;
+import hello.advanced.trace.strategy.code.strategy.StrategyLogic2;
+import lombok.extern.slf4j.Slf4j;
+import org.junit.jupiter.api.Test;
+
+@Slf4j
+public class ContextV1Test {
+
+    @Test
+    void strategyV0() {
+        logic1();
+        logic2();
+    }
+
+    private void logic2() {
+        long startTime = System.currentTimeMillis(); //비즈니스 로직 실행
+        log.info("비즈니스 로직1 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    private void logic1() {
+        long startTime = System.currentTimeMillis(); //비즈니스 로직 실행
+        log.info("비즈니스 로직2 실행");
+        //비즈니스 로직 종료
+        long endTime = System.currentTimeMillis();
+        long resultTime = endTime - startTime;
+        log.info("resultTime={}", resultTime);
+    }
+
+    @Test
+    void strategyV1() {
+        Strategy strategyLogic1 = new StrategyLogic1();
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new StrategyLogic2();
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    @Test
+    void strategyV2() {     // 익명 내부 클래스 이용
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행 - 익명 내부 클래스 사용");
+            }
+        };
+        log.info("strategyLogic1 = {}", strategyLogic1.getClass());
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행 - 익명 내부 클래스 사용");
+            }
+        };
+        log.info("strategyLogic2 = {}", strategyLogic2.getClass());
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    @Test
+    void strategyV3() {
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 1 실행 - 익명 내부 클래스 사용 2");
+            }
+        });
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직 2 실행 - 익명 내부 클래스 사용 2");
+            }
+        });
+        context2.execute();
+    }
+
+    @Test
+    void strategyV4() { // 익명 내부 클래스, 람다
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행 - 람다"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행 - 람다"));
+        context2.execute();
+    }
+}
---
+++ src/test/java/hello/advanced/trace/strategy/code/strategy/Strategy.java
@@ -0,0 +1,8 @@
+package hello.advanced.trace.strategy.code.strategy;
+
+/*
+* Strategy 인터페이스는 변하는 알고리즘 역할을 한다.
+* */
+public interface Strategy {
+    void call();
+}
